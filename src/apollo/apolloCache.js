5b6ba6e20159a3ab5e580ed5312aa6798e3405e7
Delphin Barraud <delphin.barraud@gmail.com>
2022-06-02 10:25:10 +0200

    Fix the "user not found" issue while loading
    Since we send back an array from the cache in case of read of single user, we have two situations:
     - user reference is in the cache => user is found and it works immediately
      - user reference is not in cache and the useQuery return an array with 0 element (query is sent during next tick)
    
    In the second case, instead of displaying "user not found" we want to display "loading..." and hope that we won't be actually having no result (because in this case, we will have an infinite "loading...")!!
    
    Note: A solution for this second issue could be that the back-end send back an error if user is not found

--- src/apollo/apolloCache.js
+++ src/apollo/apolloCache.js
@@ -1,30 +1,32 @@
 import { InMemoryCache } from "@apollo/client";
 
 const apolloCache = new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          user: {
-            read(_, { args, toReference }) {
-              return toReference({
-                __typename: 'User',
-                id: args.id,
-              });
-            }
-          },
-          users: {
-            read(_, { args, toReference }) {
-              if (typeof args?.filters?.id === "string") {
-                return [toReference({
-                  __typename: 'User',
-                  id: args.filters.id,
-                })];
-              }
-            }
-          }
-        }
-      }
-    }
-  });
+		typePolicies: {
+			Query: {
+				fields: {
+					user: {
+						read(_, { args, toReference }) {
+							return toReference({
+								__typename: 'User',
+								id: args.id,
+							});
+						}
+					},
+					users: {
+						read(_, { args, toReference }) {
+							if (typeof args?.filters?.id === "string") {
+								return [
+									toReference({
+										__typename: 'User',
+										id: args.filters.id,
+									})
+								];
+							}
+						}
+					}
+				}
+			}
+		}
+	});
 
 export default apolloCache;
