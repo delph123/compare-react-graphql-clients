489a7793872a9c310c9ddceb5ee3b0c16f08107e
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-05 18:08:46 +0200

    Erroneous version with single cache entry (for last query only)
    This version is affected by a race conditions on query response time: the last query response will be the one display, even though it's not the last requested one.
    
    For example, when navigating between number of friends, this version can display an incorrect number of frinds (from a pair number, increment quickly twice and the result is the previous number of users)

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,5 +1,5 @@
 import { OperationVariables, QueryHookOptions, QueryOptions, QueryResult, TypedDocumentNode, useQuery as useApolloQuery } from "@apollo/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { client as apolloClient } from "../apollo/ApolloWrapper";
 
 const USE_APOLLO_HOOK = false;
@@ -9,39 +9,32 @@ function useReduxQuery<TData = any, TVariables = OperationVariables>(
     options?: QueryHookOptions<TData, TVariables>
 ): QueryResult<TData, TVariables> {
 
-    const [cache, setCache] = useState({} as Record<string, QueryResult<TData, TVariables>>);
+    const [cache, setCache] = useState({ loading: true } as QueryResult<TData, TVariables>);
 
     const queryId = (query.definitions[0] as any).name.value + ":" + JSON.stringify(options?.variables);
-    const result = cache[queryId];
 
-    console.log("Called useQuery on:", queryId, result);
+    console.log("Called useQuery on:", queryId, cache);
 
-    if (result == null) {
+    useEffect(() => {
         console.log("Launching query:", queryId);
+        setCache({ loading: true } as QueryResult<TData, TVariables>);
         // Launch query
         apolloClient.query({
             ...options,
             query,
         } as QueryOptions<TVariables, TData>).then((result) => {
             console.log("Got result:", result, queryId);
-            setCache({
-                ...cache,
-                [queryId]: result as QueryResult<TData, TVariables>,
-            });
+            setCache(result as QueryResult<TData, TVariables>);
         }).catch((error) => {
             console.log("Got error:", error, queryId);
             setCache({
-                ...cache,
-                [queryId]: {
-                    loading: false,
-                    error,
-                } as QueryResult<TData, TVariables>,
-            });
+                loading: false,
+                error,
+            } as QueryResult<TData, TVariables>);
         });
-        return { loading: true } as QueryResult<TData, TVariables>;
-    } else {
-        return result;
-    }
+    }, [queryId]);
+
+    return cache;
 }
 
 const useQuery = USE_APOLLO_HOOK ? useApolloQuery : useReduxQuery;
