a902fe4267fdf7b9ef4fe2811df10849b633cb26
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-05 23:45:42 +0200

    Add Apollo & global cache clear/reset

--- src/apollo/localState.ts
+++ src/apollo/localState.ts
@@ -1,6 +1,24 @@
 import { makeVar, QueryResult } from "@apollo/client";
+import { client } from "./ApolloWrapper";
 
 export const backgroundColorVar = makeVar('#E6E6FA');
 export const numberOfFriendsVar = makeVar(10);
 
 export const globalQueryCache = makeVar({} as Record<string, QueryResult>);
+
+export function clearGlobalQueryCache() {
+    globalQueryCache({});
+}
+
+export function resetApolloCache() {
+    client.resetStore();
+}
+
+export function clearApolloCache() {
+    client.clearStore();
+}
+
+export function refresh(userId?: string) {
+    clearApolloCache();
+    clearGlobalQueryCache();
+}
--- src/page/UserDetails.tsx
+++ src/page/UserDetails.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
 import { backgroundColorVar } from "../apollo/localState";
 import Friends from "../component/Friends";
 import Header from "../component/Header";
@@ -7,6 +7,10 @@ import User from "../component/User";
 import useLocalState from "../hooks/useLocalState";
 
 interface UserDetailsProps {}
+
+function navigateUserId(navigate: NavigateFunction, prevUserId: string, nav: (prevUserId: number) => number) {
+  return () => navigate(`/user/${nav(parseInt(prevUserId))}`);
+}
 
 const UserDetails: FC<UserDetailsProps> = function () {
     const [color, setColor] = useLocalState(backgroundColorVar);
@@ -16,9 +20,15 @@ const UserDetails: FC<UserDetailsProps> = function () {
   
     return (
       <>
-        <Header color={color} onColorChanged={setColor} onNavigationPressed={(nav) => navigate(`/user/${nav(parseInt(params.userId))}`)}>
+        <Header color={color} onColorChanged={setColor}>
             <User userId={userId} noLink={true} />
-            <button onClick={() => navigate("/")}>Home</button>
+            <div className="App-header-bar">
+              <button onClick={navigateUserId(navigate, params.userId, n => n-10)}>&lt;&lt;&nbsp;previous (10)</button>
+              <button onClick={navigateUserId(navigate, params.userId, n => n-1)}>&lt;&nbsp;previous</button>
+              <button onClick={() => navigate("/")}>Home</button>
+              <button onClick={navigateUserId(navigate, params.userId, n => n+1)}>next&nbsp;&gt;</button>
+              <button onClick={navigateUserId(navigate, params.userId, n => n+10)}>next (10)&nbsp;&gt;&gt;</button>
+            </div>
         </Header>
         <div className="App">
             <Friends userId={userId} backgroundColor={color} />
