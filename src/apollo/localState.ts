b11baa06ce5922e391b62964765eccfbcd969566
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-05 19:09:21 +0200

    Migrate useReduxQuery to a global store
    Instead of using useState to store useReduxQuery's response cache, use a global store via a reactive variable and useLocalState hook

--- src/apollo/localState.ts
+++ src/apollo/localState.ts
@@ -1,4 +1,6 @@
-import { makeVar } from "@apollo/client";
+import { makeVar, QueryResult } from "@apollo/client";
 
 export const backgroundColorVar = makeVar('#E6E6FA');
 export const numberOfFriendsVar = makeVar(10);
+
+export const globalQueryCache = makeVar({} as Record<string, QueryResult>);
