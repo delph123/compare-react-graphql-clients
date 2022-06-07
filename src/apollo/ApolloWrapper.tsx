a026148d1439bd1ac0d6ed1e43116f2e4536018b
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-08 00:39:41 +0200

    Add option to use BatchHttpLink
    Note: option is not enabled since it's not supported on server side.

--- src/apollo/ApolloWrapper.tsx
+++ src/apollo/ApolloWrapper.tsx
@@ -1,9 +1,27 @@
 import React, { FC, PropsWithChildren } from "react";
-import { ApolloClient, ApolloProvider } from "@apollo/client";
+import { ApolloClient, ApolloProvider, HttpLink } from "@apollo/client";
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
 import apolloCache from "./apolloCache";
 
+const USE_BATCH_HTTP_LINK = false;
+const URL = "/graphql";
+
+let apolloLink = null;
+
+if (USE_BATCH_HTTP_LINK) {
+    apolloLink = new BatchHttpLink({
+        uri: URL,
+        batchMax: 10, // No more than 5 operations per batch
+        batchInterval: 10 // Wait no more than 20ms after first batched operation
+    });
+} else {
+    apolloLink = new HttpLink({
+        uri: URL,
+    });
+}
+
 export const client = new ApolloClient({
-    uri: '/graphql',
+    link: apolloLink,
     cache: apolloCache
 });
 
