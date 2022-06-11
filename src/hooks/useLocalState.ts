a85d90245168199fdaf11a3246b7d8b0bc41c8f5
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-11 11:17:04 +0200

    Install react query

--- src/hooks/useLocalState.ts
+++ src/hooks/useLocalState.ts
@@ -1,27 +1,27 @@
 import { useReactiveVar, ReactiveVar } from "@apollo/client";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 const USE_REACT_STATE = false;
 
 function setReactiveVar<T>(variable: ReactiveVar<T>) {
-    return function (value: SetStateAction<T>) {
-        if (typeof value === "function") {
-            const computeValue = value as (prevState: T) => T;
-            variable(computeValue(variable()));
-        } else {
-            variable(value);
-        }
-    }
+	return function (value: SetStateAction<T>) {
+		if (typeof value === "function") {
+			const computeValue = value as (prevState: T) => T;
+			variable(computeValue(variable()));
+		} else {
+			variable(value);
+		}
+	}
 }
 
 function useReactiveVarState<T>(variable: ReactiveVar<T>): [T, Dispatch<SetStateAction<T>>] {
-    const value = useReactiveVar(variable);
-    return [value, setReactiveVar(variable)];
+	const value = useReactiveVar(variable);
+	return [value, setReactiveVar(variable)];
 }
 
 function useReactState<T>(variable: ReactiveVar<T>): [T, Dispatch<SetStateAction<T>>] {
-    const initialValue = variable();
-    return useState(initialValue);
+	const initialValue = variable();
+	return useState(initialValue);
 }
 
 const useLocalState = USE_REACT_STATE ? useReactState : useReactiveVarState;
