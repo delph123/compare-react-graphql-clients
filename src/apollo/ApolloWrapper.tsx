9dfd62484635e19d2af5b33ac27b1472a10f15ee
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-12 22:38:58 +0200

    Use URL parameters to configure hooks

--- src/apollo/ApolloWrapper.tsx
+++ src/apollo/ApolloWrapper.tsx
@@ -2,37 +2,33 @@ import React, { FC, PropsWithChildren } from "react";
 import { ApolloClient, ApolloProvider, HttpLink } from "@apollo/client";
 import { BatchHttpLink } from "@apollo/client/link/batch-http";
 import apolloCache from "./apolloCache";
+import { USE_BATCH_HTTP_LINK } from "../config/parameters";
 
-const USE_BATCH_HTTP_LINK = false;
 const URL = "/graphql";
 
 let apolloLink = null;
 
 if (USE_BATCH_HTTP_LINK) {
-    apolloLink = new BatchHttpLink({
-        uri: URL,
-        batchMax: 10, // No more than 5 operations per batch
-        batchInterval: 10 // Wait no more than 20ms after first batched operation
-    });
+	apolloLink = new BatchHttpLink({
+		uri: URL,
+		batchMax: 10, // No more than 5 operations per batch
+		batchInterval: 10, // Wait no more than 20ms after first batched operation
+	});
 } else {
-    apolloLink = new HttpLink({
-        uri: URL,
-    });
+	apolloLink = new HttpLink({
+		uri: URL,
+	});
 }
 
 export const client = new ApolloClient({
-    link: apolloLink,
-    cache: apolloCache
+	link: apolloLink,
+	cache: apolloCache,
 });
 
 type ApolloWrapperProps = PropsWithChildren<{}>;
 
 const ApolloWrapper: FC<ApolloWrapperProps> = function ({ children }) {
-    return (
-        <ApolloProvider client={client}>
-            {children}
-        </ApolloProvider>
-    );
-}
+	return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
 
 export default ApolloWrapper;
