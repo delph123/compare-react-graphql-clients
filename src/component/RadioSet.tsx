f85f81d117110c21c1dc3d07d095760305594e6b
Delphin Barraud <delphin.barraud@gmail.com>
2022-06-12 22:01:09 +0200

    Add two radio button state for managing parameters

---
+++ src/component/RadioSet.tsx
@@ -0,0 +1,42 @@
+import React, { ChangeEventHandler, FC } from "react";
+
+interface RadioSetProps {
+	className: string;
+	name: string;
+	legend?: string;
+	choices: {
+		id: string;
+		label: string;
+	}[];
+	selected: string;
+	onSelect?: ChangeEventHandler<HTMLInputElement>;
+}
+
+const RadioSet: FC<RadioSetProps> = function RadioSet({
+	className,
+	name,
+	legend,
+	choices,
+	selected,
+	onSelect,
+}) {
+	return (
+		<fieldset className={className}>
+			{legend && <legend>{legend}</legend>}
+			{choices.map((choice) => (
+				<div key={choice.id}>
+					<input
+						type="radio"
+						name={name}
+						id={choice.id}
+						checked={selected === choice.id}
+						onChange={onSelect}
+					/>
+					<label htmlFor={choice.id}>{choice.label}</label>
+				</div>
+			))}
+		</fieldset>
+	);
+};
+
+export default RadioSet;
