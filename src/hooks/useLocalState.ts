335c91c719b332ad3310653563f1013c5daf8840
Delphin Barraud <delphin.barraud@soprasteria.com>
2022-06-18 22:45:41 +0200

    First simple implementation of query cache in redux

--- src/hooks/useLocalState.ts
+++ src/hooks/useLocalState.ts
@@ -1,15 +1,9 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useReactiveVar, ReactiveVar } from "@apollo/client";
 import { Dispatch, SetStateAction, useState } from "react";
-import { backgroundColorVar, numberOfFriendsVar } from "../apollo/localState";
 import { USE_STORE_LIBRARY } from "../config/parameters";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import {
-	backgroundColorSelector,
-	incrementNumberOfFriendsByAmount,
-	numberOfFriendsSelector,
-	setBackgroundColor,
-} from "../redux/slices/localStateSlice";
+import getReduxInterfaceMapping from "../redux/interfaceMapping";
 
 function setReactiveVar<T>(variable: ReactiveVar<T>) {
 	return function (value: SetStateAction<T>) {
@@ -39,30 +33,13 @@ function useReactState<T>(
 function useReduxState<T>(
 	variable: ReactiveVar<T>
 ): [T, Dispatch<SetStateAction<T>>] {
-	switch (variable) {
-		// @ts-ignore
-		case backgroundColorVar:
-			const bgColor = useAppSelector(backgroundColorSelector);
-			const dispatchBgColor = useAppDispatch();
-			const setBgColor = (action: string) => {
-				dispatchBgColor(setBackgroundColor(action));
-			};
-			// @ts-ignore
-			return [bgColor, setBgColor];
-		// @ts-ignore
-		case numberOfFriendsVar:
-			const nbFriends = useAppSelector(numberOfFriendsSelector);
-			const dispatchNbFriends = useAppDispatch();
-			const setNbFriends = (computeAmount: (n: number) => number) => {
-				dispatchNbFriends(
-					incrementNumberOfFriendsByAmount(computeAmount(0))
-				);
-			};
-			// @ts-ignore
-			return [nbFriends, setNbFriends];
-		default:
-			return useReactiveVarState(variable);
-	}
+	const { selector, actionCreator } = getReduxInterfaceMapping(variable);
+	const dispatch = useAppDispatch();
+	const value = useAppSelector(selector);
+	const setValue = (setStateAction: SetStateAction<T>) => {
+		dispatch(actionCreator(setStateAction));
+	};
+	return [value, setValue];
 }
 
 const useLocalState = { useReactState, useReactiveVarState, useReduxState }[
---
+++ src/redux/interfaceMapping.ts
@@ -0,0 +1,70 @@
+import { QueryResult, ReactiveVar } from "@apollo/client";
+import { AnyAction } from "@reduxjs/toolkit";
+import { SetStateAction } from "react";
+import {
+	backgroundColorVar,
+	globalQueryCache,
+	numberOfFriendsVar,
+} from "../apollo/localState";
+import {
+	backgroundColorSelector,
+	incrementNumberOfFriendsByAmount,
+	numberOfFriendsSelector,
+	setBackgroundColor,
+} from "./slices/localStateSlice";
+import { addToCache, queryCacheSelector } from "./slices/queryCacheSlice";
+import { RootState } from "./store";
+
+export interface ReduxInterface<T> {
+	selector: (state: RootState) => T;
+	actionCreator: (action: SetStateAction<T>) => AnyAction;
+}
+
+const reduxMapping = new Map<ReactiveVar<any>, ReduxInterface<any>>();
+
+function setReduxInterfactMapping<T>(
+	variable: ReactiveVar<T>,
+	reduxInterface: ReduxInterface<T>
+) {
+	reduxMapping.set(variable, reduxInterface);
+}
+
+export default function getReduxInterfaceMapping<T>(
+	variable: ReactiveVar<T>
+): ReduxInterface<T> {
+	const intf = reduxMapping.get(variable);
+	if (intf != null) {
+		return intf;
+	} else {
+		throw new Error("Redux interface mapping not found!!");
+	}
+}
+
+// Background Color
+setReduxInterfactMapping(backgroundColorVar, {
+	selector: backgroundColorSelector,
+	actionCreator(color) {
+		return setBackgroundColor(color as string);
+	},
+});
+
+// Number of Friends
+setReduxInterfactMapping(numberOfFriendsVar, {
+	selector: numberOfFriendsSelector,
+	actionCreator(action) {
+		const computeAmount = action as (n: number) => number;
+		return incrementNumberOfFriendsByAmount(computeAmount(0));
+	},
+});
+
+// Query Cache
+setReduxInterfactMapping(globalQueryCache, {
+	selector: queryCacheSelector,
+	actionCreator(action) {
+		const cacheAdder = action as (
+			prevState: Record<string, QueryResult>
+		) => Record<string, QueryResult>;
+		const [queryId, result] = Object.entries(cacheAdder({}))[0];
+		return addToCache({ queryId, result });
+	},
+});
