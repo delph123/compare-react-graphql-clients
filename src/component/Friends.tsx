9c9b006e816c8209a2f71c1f41c7ceed2bea4e74
Delphin Barraud <delphin.barraud@soprasteria.com>
2022-06-18 18:30:47 +0200

    Add redux-toolkit (used in Friends component)

--- src/component/Friends.tsx
+++ src/component/Friends.tsx
@@ -4,50 +4,90 @@ import useLocalState from "../hooks/useLocalState";
 import useQuery from "../hooks/useQuery";
 import useRenderCounter from "../hooks/useRenderCounter";
 import { GetFriends } from "../queries/users";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import {
+	decrementNumberOfFriends,
+	incrementNumberOfFriends,
+	numberOfFriendsSelector,
+} from "../redux/slices/localStateSlice";
 import Loading from "./Loading";
 import User from "./User";
 
 interface FriendsProps {
-    userId: string;
-    backgroundColor?: string;
+	userId: string;
+	backgroundColor?: string;
 }
 
-const increment = (n : number) => n + 1;
-const decrement = (n : number) => n - 1;
+const increment = (n: number) => n + 1;
+const decrement = (n: number) => n - 1;
 
 // Friends is a Pure Components w.r.t. its props so we can encaspsulate it
 // in React.memo to avoid re-rendering when no prop has changed.
-const Friends: FC<FriendsProps> = React.memo(function Friends({ userId, backgroundColor = 'magenta' }) {
-    const renderCounter  = useRenderCounter();
+const Friends: FC<FriendsProps> = React.memo(function Friends({
+	userId,
+	backgroundColor = "magenta",
+}) {
+	const renderCounter = useRenderCounter();
 
-    const [nbFriends, setNbFriends] = useLocalState(numberOfFriendsVar);
+	// const [nbFriends, setNbFriends] = useLocalState(numberOfFriendsVar);
+	const dispatch = useAppDispatch();
+	const nbFriends = useAppSelector(numberOfFriendsSelector);
+	const setNbFriends = (action: (n: number) => number) => {
+		if (action === increment) {
+			dispatch(incrementNumberOfFriends());
+		}
+		if (action === decrement) {
+			dispatch(decrementNumberOfFriends());
+		}
+	};
 
-    const { data: { user: { friends } } = { user: { friends: [] } }, loading, error } = useQuery(GetFriends, {
-        variables: {
-            userId,
-            nbFriends
-          }
-    });
+	const {
+		data: { user: { friends } } = { user: { friends: [] } },
+		loading,
+		error,
+	} = useQuery(GetFriends, {
+		variables: {
+			userId,
+			nbFriends,
+		},
+	});
 
-    if (error) return <div><p>Error :-/</p><p>Caused by: {error.message}</p></div>;
+	if (error)
+		return (
+			<div>
+				<p>Error :-/</p>
+				<p>Caused by: {error.message}</p>
+			</div>
+		);
 
-    return (
-        <section className='App-section' style={{
-            backgroundColor: backgroundColor
-        }}>
-            <div className="App-section-title">
-                [{renderCounter}]
-                <h2>FRIENDS ({nbFriends})</h2>
-                <button onClick={() => setNbFriends(increment)}>+</button>
-                <button onClick={() => setNbFriends(decrement)}>-</button>
-            </div>
-            {loading ? <p><Loading /></p> : friends.map((friend: any, i: number) => {
-                return (
-                    <p key={friend.uuid}>({i+1}/{friends.length}) <User userId={friend.uuid} /></p>
-                );
-            })}
-        </section>
-    );
+	return (
+		<section
+			className="App-section"
+			style={{
+				backgroundColor: backgroundColor,
+			}}
+		>
+			<div className="App-section-title">
+				[{renderCounter}]<h2>FRIENDS ({nbFriends})</h2>
+				<button onClick={() => setNbFriends(increment)}>+</button>
+				<button onClick={() => setNbFriends(decrement)}>-</button>
+			</div>
+			{loading ? (
+				<p>
+					<Loading />
+				</p>
+			) : (
+				friends.map((friend: any, i: number) => {
+					return (
+						<p key={friend.uuid}>
+							({i + 1}/{friends.length}){" "}
+							<User userId={friend.uuid} />
+						</p>
+					);
+				})
+			)}
+		</section>
+	);
 });
 
 export default Friends;
