57cdf29cea42ab1855dac67750640a59585777f0
Delphin Barraud <delphin.barraud@soprasteria.com>
2022-06-19 00:12:47 +0200

    Display Users during loading when there's data

--- src/component/Users.tsx
+++ src/component/Users.tsx
@@ -6,44 +6,69 @@ import Loading from "./Loading";
 import User from "./User";
 
 interface UsersProps {
-    ageAbove?: number;
-    ageBelow?: number;
-    category?: string[];
-    backgroundColor?: string;
+	ageAbove?: number;
+	ageBelow?: number;
+	category?: string[];
+	backgroundColor?: string;
 }
 
 // Users is a Pure Components w.r.t. its props so we can encaspsulate it
 // in React.memo to avoid re-rendering when no prop has changed.
-const Users: FC<UsersProps> = React.memo(function Users({ ageAbove, ageBelow, category, backgroundColor = 'magenta' }) {
-    const renderCounter  = useRenderCounter();
+const Users: FC<UsersProps> = React.memo(function Users({
+	ageAbove,
+	ageBelow,
+	category,
+	backgroundColor = "magenta",
+}) {
+	const renderCounter = useRenderCounter();
 
-    const { data, loading, error, refetch } = useQuery(GetUsers, {
-        variables: {
-            filters: {
-                ageAbove: ageAbove,
-                ageBelow: ageBelow,
-                types: category
-            }
-        }
-    });
+	const { data, loading, error, refetch } = useQuery(GetUsers, {
+		variables: {
+			filters: {
+				ageAbove: ageAbove,
+				ageBelow: ageBelow,
+				types: category,
+			},
+		},
+	});
 
-    if (error) return <div><p>Error :-/</p><p>Caused by: {error.message}</p></div>;
+	if (error)
+		return (
+			<div>
+				<p>Error :-/</p>
+				<p>Caused by: {error.message}</p>
+			</div>
+		);
 
-    return (
-        <section className='App-section' style={{
-            backgroundColor: backgroundColor
-        }}>
-            [{renderCounter}]
-            <h2 className="App-section-title">{!category ? "ALL" : category.join(' + ')} {!ageAbove ? "" : `(above ${ageAbove})`}{!ageBelow ? "" : `(below ${ageBelow})`}</h2>
-            <button onClick={() => refetch()}>Refresh</button>
-            {loading ? <p><Loading /></p> : data.users.map((user: any) => {
-                return (
-                    <p key={user.uuid}><User userId={user.uuid} /></p>
-                )
-            })}
-        </section>
-    );
-
+	return (
+		<section
+			className="App-section"
+			style={{
+				backgroundColor: backgroundColor,
+			}}
+		>
+			[{renderCounter}]
+			<h2 className="App-section-title">
+				{!category ? "ALL" : category.join(" + ")}{" "}
+				{!ageAbove ? "" : `(above ${ageAbove})`}
+				{!ageBelow ? "" : `(below ${ageBelow})`}
+			</h2>
+			<button onClick={() => refetch()}>Refresh</button>
+			{!data && loading ? (
+				<p>
+					<Loading />
+				</p>
+			) : (
+				data.users.map((user: any) => {
+					return (
+						<p key={user.uuid}>
+							<User userId={user.uuid} />
+						</p>
+					);
+				})
+			)}
+		</section>
+	);
 });
 
 export default Users;
