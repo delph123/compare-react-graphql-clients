5ecbb2eaa6c2bd1697442ea29dd851f1f1fc7c2c
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-06-19 14:59:59 +0200

    Retain URL search params during navigation

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,51 +1,80 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
+import Link from "./Link";
 import useQuery from "../hooks/useQuery";
 import useRenderCounter from "../hooks/useRenderCounter";
 import { GetUser } from "../queries/users";
 import Loading from "./Loading";
 
 interface UserProps {
-    userId: string;
-    noLink?: boolean;
+	userId: string;
+	noLink?: boolean;
 }
 
 // User is a Pure Components w.r.t. its props so we can encaspsulate it
 // in React.memo to avoid re-rendering when no prop has changed.
-const User: FC<UserProps> = React.memo(function User({ userId, noLink = false }) {
-    const renderCounter = useRenderCounter();
+const User: FC<UserProps> = React.memo(function User({
+	userId,
+	noLink = false,
+}) {
+	const renderCounter = useRenderCounter();
 
-    let { data: { user, users } = {}, loading, error } = useQuery(GetUser, {
-        variables: {
-            userId
-          }
-    });
+	let {
+		data: { user, users } = {},
+		loading,
+		error,
+	} = useQuery(GetUser, {
+		variables: {
+			userId,
+		},
+	});
 
-    if (loading) return <Loading />;
-    if (error) return <div><p>Error :-/</p><p>Caused by: {error.message}</p></div>;
+	if (loading) return <Loading />;
+	if (error)
+		return (
+			<div>
+				<p>Error :-/</p>
+				<p>Caused by: {error.message}</p>
+			</div>
+		);
 
-    if (users && !user) {
-        user = users[0];
-    }
+	if (users && !user) {
+		user = users[0];
+	}
 
-    if (user == null) {
-        return (
-            <i>
-                User&nbsp;
-                <Link className="App-user-id" to={`/user/${userId.substring(5)}`}>{userId}</Link>
-                &nbsp;not found!!
-            </i>
-        );
-    }
+	if (user == null) {
+		return (
+			<i>
+				User&nbsp;
+				<Link
+					className="App-user-id"
+					to={`/user/${userId.substring(5)}`}
+				>
+					{userId}
+				</Link>
+				&nbsp;not found!!
+			</i>
+		);
+	}
 
-    return (
-        <>
-            {!noLink ? <Link className="App-user-id" to={`/user/${userId.substring(5)}`}>{userId}:</Link> : null}
-            &nbsp;
-            {`${user.firstName} ${user.lastName} (${user.type.substring(0, 1).toUpperCase()}${user.type.substring(1).toLowerCase()}) - ${user.age} y.o.`}
-            {` [${renderCounter}]`}
-        </>
-    );
+	return (
+		<>
+			{!noLink ? (
+				<Link
+					className="App-user-id"
+					to={`/user/${userId.substring(5)}`}
+				>
+					{userId}:
+				</Link>
+			) : null}
+			&nbsp;
+			{`${user.firstName} ${user.lastName} (${user.type
+				.substring(0, 1)
+				.toUpperCase()}${user.type.substring(1).toLowerCase()}) - ${
+				user.age
+			} y.o.`}
+			{` [${renderCounter}]`}
+		</>
+	);
 });
 
 export default User;
--- src/page/UserDetails.tsx
+++ src/page/UserDetails.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from "react";
-import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
+import {
+	NavigateFunction,
+	useLocation,
+	useNavigate,
+	useParams,
+} from "react-router-dom";
 import { backgroundColorVar } from "../apollo/localState";
 import Friends from "../component/Friends";
 import Header from "../component/Header";
@@ -8,33 +13,75 @@ import useLocalState from "../hooks/useLocalState";
 
 interface UserDetailsProps {}
 
-function navigateUserId(navigate: NavigateFunction, prevUserId: string, nav: (prevUserId: number) => number) {
-  return () => navigate(`/user/${nav(parseInt(prevUserId))}`);
+function navigateUserId(
+	navigate: NavigateFunction,
+	search: string,
+	prevUserId: string,
+	nav: (prevUserId: number) => number
+) {
+	return () => navigate(`/user/${nav(parseInt(prevUserId))}${search}`);
 }
 
 const UserDetails: FC<UserDetailsProps> = function () {
-    const [color, setColor] = useLocalState(backgroundColorVar);
-    const params = useParams() as { userId: string };
-    const navigate = useNavigate();
-    const userId = `user-${params.userId}`;
+	const [color, setColor] = useLocalState(backgroundColorVar);
+	const params = useParams() as { userId: string };
+	const navigate = useNavigate();
+	const { search } = useLocation();
+	const userId = `user-${params.userId}`;
 
-    return (
-      <>
-        <Header color={color} onColorChanged={setColor}>
-            <User userId={userId} noLink={true} />
-            <div className="App-header-bar">
-              <button onClick={navigateUserId(navigate, params.userId, n => n-10)}>&lt;&lt;&nbsp;previous (10)</button>
-              <button onClick={navigateUserId(navigate, params.userId, n => n-1)}>&lt;&nbsp;previous</button>
-              <button onClick={() => navigate("/")}>Home</button>
-              <button onClick={navigateUserId(navigate, params.userId, n => n+1)}>next&nbsp;&gt;</button>
-              <button onClick={navigateUserId(navigate, params.userId, n => n+10)}>next (10)&nbsp;&gt;&gt;</button>
-            </div>
-        </Header>
-        <div className="App">
-            <Friends userId={userId} backgroundColor={color} />
-        </div>
-      </>
-    );
-}
+	return (
+		<>
+			<Header color={color} onColorChanged={setColor}>
+				<User userId={userId} noLink={true} />
+				<div className="App-header-bar">
+					<button
+						onClick={navigateUserId(
+							navigate,
+							search,
+							params.userId,
+							(n) => n - 10
+						)}
+					>
+						&lt;&lt;&nbsp;previous (10)
+					</button>
+					<button
+						onClick={navigateUserId(
+							navigate,
+							search,
+							params.userId,
+							(n) => n - 1
+						)}
+					>
+						&lt;&nbsp;previous
+					</button>
+					<button onClick={() => navigate("/" + search)}>Home</button>
+					<button
+						onClick={navigateUserId(
+							navigate,
+							search,
+							params.userId,
+							(n) => n + 1
+						)}
+					>
+						next&nbsp;&gt;
+					</button>
+					<button
+						onClick={navigateUserId(
+							navigate,
+							search,
+							params.userId,
+							(n) => n + 10
+						)}
+					>
+						next (10)&nbsp;&gt;&gt;
+					</button>
+				</div>
+			</Header>
+			<div className="App">
+				<Friends userId={userId} backgroundColor={color} />
+			</div>
+		</>
+	);
+};
 
 export default UserDetails;
