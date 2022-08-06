5106510bf97610445cc9e9d784c9e176421bdcbf
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-08-06 23:04:31 +0200

    Add users settings state in redux

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ To be filled...
 ## To Do
 
 -   Write README.md
--   Rework redux selectors to avoid multi-refresh
+-   Rework redux selectors to avoid multi-refresh (while changing color for example)
 -   Avoid friends.length for getting nbFriends
--   Implement refresh buttons
+-   Implement refresh button in header for redux
 -   Adjust global store eviction as well according to same fine-grained eviction as apollo cache
--   Migrate useReduxQuery to Redux
 -   Add Babel compilation for GraphQL queries & write queries as .graphql, add client-side schema
--- src/redux/slices/localStateSlice.ts
+++ src/redux/slices/localStateSlice.ts
@@ -1,14 +1,34 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { SettingsParameters } from "../../component/SettingsDialog";
 import { RootState } from "../store";
+
+export type Sides = "left" | "middle" | "right";
 
 export interface LocalState {
 	backgroundColor: string;
 	numberOfFriends: number;
+	usersSettings: {
+		left: SettingsParameters;
+		middle: SettingsParameters;
+		right: SettingsParameters;
+	};
 }
 
 const initialState: LocalState = {
 	backgroundColor: "#E6E6FA",
 	numberOfFriends: 10,
+	usersSettings: {
+		left: {
+			ageBelow: 34,
+			categories: ["HUMAN"],
+		},
+		middle: {
+			categories: ["ADMIN", "COMPUTER"],
+		},
+		right: {
+			ageAbove: 30,
+		},
+	},
 };
 
 export const localStateSlice = createSlice({
@@ -21,17 +41,30 @@ export const localStateSlice = createSlice({
 		setBackgroundColor(state, action: PayloadAction<string>) {
 			state.backgroundColor = action.payload;
 		},
+		setUsersSettings(
+			state,
+			{
+				payload: { side, settings },
+			}: PayloadAction<{ side: Sides; settings: SettingsParameters }>
+		) {
+			state.usersSettings[side] = settings;
+		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { incrementNumberOfFriendsByAmount, setBackgroundColor } =
-	localStateSlice.actions;
+export const {
+	incrementNumberOfFriendsByAmount,
+	setBackgroundColor,
+	setUsersSettings,
+} = localStateSlice.actions;
 
 // Selectors
 export const numberOfFriendsSelector = (state: RootState) =>
 	state.localState.numberOfFriends;
 export const backgroundColorSelector = (state: RootState) =>
 	state.localState.backgroundColor;
+export const usersSettingsSelector = (side: Sides) => (state: RootState) =>
+	state.localState.usersSettings[side];
 
 export default localStateSlice.reducer;
