9d24634a2d08e53173f94e5802f0ccdbf9803d65
Delphin Barraud <delphin.barraud@gmail.com>
2022-08-18 19:05:17 +0200

    Switching to vite for build & proxy as well

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	server: {
+		port: 8000,
+		open: true,
+		proxy: {
+			"/graphql": "http://localhost:4000",
+		},
+	},
+	build: {
+		outDir: "build",
+	},
+});
