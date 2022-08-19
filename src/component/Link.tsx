14507c6c4f231b88c12a2cea01b49e00524779da
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-08-19 15:12:02 +0200

    Refactor unit test for <Link> and fix missing cases

--- src/component/Link.tsx
+++ src/component/Link.tsx
@@ -1,5 +1,18 @@
 import { FC } from "react";
-import { Link as RouterLink, useLocation } from "react-router-dom";
+import { Link as RouterLink, Path, useLocation } from "react-router-dom";
+
+function concat(search: string, append: string) {
+	return search + (search.includes("?") ? append.replace("?", "&") : append);
+}
+
+function convert(url: string): Partial<Path> {
+	let [, pathname, search, hash] = /([^?#]*)(\?[^#]*)?(#.*)?/.exec(url) || [];
+	return {
+		pathname,
+		search,
+		hash,
+	};
+}
 
 const Link: FC<Parameters<typeof RouterLink>[0]> = ({
 	children,
@@ -8,8 +21,15 @@ const Link: FC<Parameters<typeof RouterLink>[0]> = ({
 }) => {
 	const { search } = useLocation();
 
+	let target = typeof to === "string" ? convert(to) : to;
+
+	target = {
+		...target,
+		search: target.search ? concat(target.search, search) : search,
+	};
+
 	return (
-		<RouterLink to={to + search} {...props}>
+		<RouterLink to={target} {...props}>
 			{children}
 		</RouterLink>
 	);
